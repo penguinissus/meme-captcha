e3f1504f962af2a233c3fd44067e794de6d66dac
Jocelyn Lau <jocelyn.ty.lau@gmail.com>
2026-02-12 17:33:20 -0500

    limit draggable area

--- src/hellofx/DraggableMaker.java
+++ src/hellofx/DraggableMaker.java
@@ -1,11 +1,11 @@
 package hellofx;
 
-import javafx.scene.Node;
+import javafx.scene.image.*;
 
 public class DraggableMaker {
     private double mouseX, mouseY;
 
-    public void makeDraggable(Node node){
+    public void makeDraggable(ImageView node){
         node.setOnMousePressed(mouseEvent -> {
             mouseX = mouseEvent.getSceneX();
             mouseY = mouseEvent.getSceneY();
@@ -14,8 +14,20 @@ public class DraggableMaker {
         node.setOnMouseDragged(mouseEvent -> {
             double deltaX = mouseEvent.getSceneX() - mouseX;
             double deltaY = mouseEvent.getSceneY() - mouseY;
-            node.setTranslateX(node.getTranslateX() + deltaX);
-            node.setTranslateY(node.getTranslateY() + deltaY);
+            if(node.getTranslateX() > -250 && node.getTranslateX() < 250){
+                node.setTranslateX(node.getTranslateX() + deltaX);
+            } else if (node.getTranslateX() <= -250){
+                node.setTranslateX(-249);
+            } else {
+                node.setTranslateX(249);
+            }
+            if(node.getTranslateY() > -250 && node.getTranslateY() < -50){
+                node.setTranslateY(node.getTranslateY() + deltaY);
+            } else if (node.getTranslateY() <= -250){
+                node.setTranslateY(-249);
+            } else {
+                node.setTranslateY(-51);
+            }
             mouseX = mouseEvent.getSceneX();
             mouseY = mouseEvent.getSceneY();
         });
